5cef0f0ad3770b0b69da07c39bc9b1052536c49e
Fabien Berthoux <thoux08@gmail.com>
2021-01-26 21:10:36 +0100

    Add HTML & CSS to Show

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -24,7 +24,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :image_url)
+    params.require(:cocktail).permit(:name, :image_url, :photo)
   end
 
 end
