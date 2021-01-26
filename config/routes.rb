f458ba513cbd993a9e86203827ab7df83993bbde
Fabien Berthoux <thoux08@gmail.com>
2021-01-26 19:41:17 +0100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Un utilisateur peut voir la liste des cocktails
   # Un utilisateur peut voir les détails d'un cocktail donné, avec la dose nécessaire pour chaque ingrédient
   # Un utilisateur peut créer un nouveau cocktail
-  resources :cocktails, only: [, :show, :new, :create] do
+  resources :cocktails, only: [:show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
 
